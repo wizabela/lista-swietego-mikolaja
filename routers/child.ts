f16dd61f2c987a317189799b5c61327cc224cd47
id github: wizabela <izabela.wyrebska@gmail.com>
2022-04-11 18:10:12 +0200

    add export for types and modify path import

--- routers/child.ts
+++ routers/child.ts
@@ -2,7 +2,7 @@ import {Router} from "express";
 import {ChildRecord} from "../records/child.record";
 import {GiftRecord} from "../records/gift.record";
 import {ValidationError} from "../utils/errors";
-import {CreateChildReq, ListChildrenRes, SetGiftForChildReq} from "../types/child/child";
+import {CreateChildReq, ListChildrenRes, SetGiftForChildReq} from "../types";
 
 
 export const childRouter = Router();
--- types/child/index.ts
+++ types/child/index.ts
@@ -1,1 +1,2 @@
 export * from './child.entity';
+export * from './child';
